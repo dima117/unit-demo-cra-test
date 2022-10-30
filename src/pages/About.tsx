842daf157e3248d597b0f8859ae14220fb2b1ada
dima117 <dima117a@yandex-team.ru>
2022-10-30 09:24:59 +0600

    настройки ci (#1)
    * настройки ci
    * deploy
    * publish artifacts
    * change page text

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,7 +4,7 @@ export const About: FC = () => {
     return (
         <>
             <h1 data-testid="page-title">About</h1>
-            <p>This is an example application.</p>
+            <p>This is an example application (published by GitHub Actions).</p>
         </>
     );
 };
